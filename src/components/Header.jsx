458b39cf87af4dc1bf25198844acd8761c4876c6
Abhishek Mestri <abmestri25@gmail.com>
2022-04-27 22:20:39 +0530

    done with ui

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types"
+
+function Header({ title }) {
+  return (
+    <header>
+      <div className='container'>
+        <h2>{title}</h2>
+      </div>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: "Feedback App",
+}
+
+Header.propTypes = {
+  title: PropTypes.string,
+}
+
+export default Header
